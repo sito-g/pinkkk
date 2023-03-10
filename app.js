db51b1d73b9a18e6cdb93357b336ad9e8a846b40
Siddartha Gupta <84712951+sito-g@users.noreply.github.com>
2023-03-10 23:20:41 +0545

    Add files via upload

---
+++ app.js
@@ -0,0 +1,35 @@
+const zoom=document.querySelector(".zoom");
+const photo=document.querySelector(".photo");
+const main=document.querySelector(".main");
+const logo=document.querySelector(".logo");
+const text=document.querySelector(".text");
+const email=document.querySelector(".email");
+
+function gettext(){
+  var intext=document.querySelector(".email").value;
+  windows.alert(intext);
+}
+
+
+const t1=new TimelineMax();
+
+
+
+
+
+t1.fromTo(photo, 1.5,{ height:"0%" },{height:"100%", ease: Power2.easeInOut })
+
+.fromTo(
+  main,
+  1,
+  {x: "-150%"},
+  {x: "0%", ease: Power2.easeInOut } ,"-=1"
+)
+.fromTo(
+  text,
+  1,
+  {x: "-150%"},
+  {x: "0%", ease: Power2.easeInOut } ,"-=1"
+)
+
+
